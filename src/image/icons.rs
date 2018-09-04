fe7aaa03000347660fa427eb05fdad9c43b6e5cd
loovjo <jonathan.loov@gmail.com>
2018-09-04 08:13:05 +0200

    Don't Read the images, include_bytes them

--- src/image/icons.rs
+++ src/image/icons.rs
@@ -1,13 +1,15 @@
 use super::png_loader::PngImage;
-use std::fs::File;
+use std::io::Cursor;
+
+macro_rules! load_image {
+    ($path:expr) => {
+        PngImage::load(Cursor::new(include_bytes!(concat!("../../resources/", $path)) as &[u8])).unwrap()
+    };
+}
 
 lazy_static! {
-    pub static ref TOOL_POINT: PngImage =
-        PngImage::load(File::open("resources/tool_point.png").unwrap()).unwrap();
-    pub static ref TOOL_LINE: PngImage =
-        PngImage::load(File::open("resources/tool_line.png").unwrap()).unwrap();
-    pub static ref TOOL_CIRCLE: PngImage =
-        PngImage::load(File::open("resources/tool_circle.png").unwrap()).unwrap();
-    pub static ref TOOL_MOVER: PngImage =
-        PngImage::load(File::open("resources/tool_mover.png").unwrap()).unwrap();
+    pub static ref TOOL_POINT: PngImage = load_image!("tool_point.png");
+    pub static ref TOOL_LINE: PngImage = load_image!("tool_line.png");
+    pub static ref TOOL_CIRCLE: PngImage = load_image!("tool_circle.png");
+    pub static ref TOOL_MOVER: PngImage = load_image!("tool_mover.png");
 }
